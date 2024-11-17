9ace06d743b2a726097d5ceacbca7cf8886de89c
sheikhmishar <misharex.sezan@gmail.com>
2024-11-17 11:20:27 +0600

    leave calender role based access

--- src/pages/leave/calender/index.tsx
+++ src/pages/leave/calender/index.tsx
@@ -2,22 +2,27 @@ import { useQuery } from '@tanstack/react-query'
 import { useContext, useEffect, useMemo, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-import CalenderSlider from '../../../components/CalenderDropdown'
+import CalenderSlider from '../../../components/CalenderSlider'
+import ProtectedComponent from '../../../components/ProtectedComponent'
 import Select from '../../../components/Select'
 import Table from '../../../components/Table'
 import { BLANK_ARRAY, ROUTES } from '../../../constants/CONSTANTS'
 import ServerSITEMAP from '../../../constants/SERVER_SITEMAP'
+import { AuthContext } from '../../../contexts/auth'
 import { ToastContext } from '../../../contexts/toast'
-import generateCalender, { getDateRange } from '../../../libs'
+import generateCalender, {
+  getDateRange,
+  dateToString,
+  stringToDate
+} from '../../../libs'
 import modifiedFetch from '../../../libs/modifiedFetch'
 
 import { GetResponseType } from 'backend/@types/response'
 import { allCompanies } from 'backend/controllers/companies'
 import { allEmployeeLeaves } from 'backend/controllers/leaves'
 
-// TODO: 09 -> September, legend
-
 const LeaveCalender = () => {
+  const { self } = useContext(AuthContext)
   const { onErrorDisplayToast } = useContext(ToastContext)
 
   const [companyId, setCompanyId] = useState(-1)
@@ -28,15 +33,14 @@ const LeaveCalender = () => {
     [currentDate]
   )
   const [fromDateString, toDateString] = useMemo(
-    () =>
-      [fromDate, toDate].map(date => date.toISOString().split('T')[0]) as [
-        string,
-        string
-      ],
+    () => [fromDate, toDate].map(dateToString) as [string, string],
     [fromDate, toDate]
   )
 
-  useEffect(() => setCurrentDate(new Date(fromDateString)), [fromDateString])
+  useEffect(
+    () => setCurrentDate(stringToDate(fromDateString)),
+    [fromDateString]
+  )
 
   const { data: employeeLeaves = BLANK_ARRAY, isFetching } = useQuery({
     queryKey: [
@@ -82,22 +86,24 @@ const LeaveCalender = () => {
             setCurrentDate={setCurrentDate}
           />
         </div>
-        <div className='col-2'>
-          <Select
-            id='company'
-            label=''
-            value={companyId}
-            onChange={({ target: { value } }) =>
-              setCompanyId(parseInt(value) || -1)
-            }
-            options={[{ label: 'All', value: -1 }].concat(
-              companies.map(company => ({
-                label: company.name,
-                value: company.id
-              }))
-            )}
-          />
-        </div>
+        <ProtectedComponent rolesAllowed={['SuperAdmin', 'HR']}>
+          <div className='col-2'>
+            <Select
+              id='company'
+              label=''
+              value={companyId}
+              onChange={({ target: { value } }) =>
+                setCompanyId(parseInt(value) || -1)
+              }
+              options={[{ label: 'All', value: -1 }].concat(
+                companies.map(company => ({
+                  label: company.name,
+                  value: company.id
+                }))
+              )}
+            />
+          </div>
+        </ProtectedComponent>
         <div className='col-2'>
           {(isFetching || fetchingCompanies) && (
             <div className='ms-3 spinner-border text-primary' role='status'>
@@ -117,7 +123,9 @@ const LeaveCalender = () => {
             calender.map(({ date, month }, i) => (
               <strong className='text-primary'>
                 {date.endsWith('22') || date.endsWith('07')
-                  ? new Date(`2011-${month}-01`).toDateString().substring(4, 7)
+                  ? stringToDate(`2011-${month}-01`)
+                      .toDateString()
+                      .substring(4, 7)
                   : ''}
                 {calender[i + 1]?.date === '01' ? (
                   <>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| </>
@@ -138,73 +146,79 @@ const LeaveCalender = () => {
             )
           ])
           .concat(
-            employeeLeaves.map(employee =>
-              [
-                <Link
-                  role='button'
-                  to={
-                    ROUTES.leave.details.replace(
-                      ROUTES.leave._params.id,
-                      employee.id.toString()
-                    ) +
-                    '?' +
-                    new URLSearchParams({
-                      month: fromDateString
-                    } satisfies typeof ROUTES.leave._queries)
-                  }
-                  className='align-items-center d-flex gap-2 py-2 text-decoration-none'
-                >
-                  <img
-                    src='/favicon.png'
-                    width='50'
-                    height='50'
-                    className='object-fit-cover rounded-circle'
-                  />
-                  <div>
-                    <p
-                      style={{ fontSize: 12 }}
-                      className='fw-lighter m-0 text-info'
-                    >
-                      {employee.email}
-                    </p>
-                    <p className='fw-bold m-0 text-nowrap'>{employee.name}</p>
-                    <p
-                      style={{ fontSize: 12 }}
-                      className='fw-lighter m-0 text-muted'
-                    >
-                      {employee.designation.name}
-                    </p>
-                  </div>
-                </Link>
-              ].concat(
-                calender.map(({ month, date }) => {
-                  const year =
-                    month === '01'
-                      ? toDate.getFullYear()
-                      : fromDate.getFullYear()
-                  const targetDate = new Date(`${year}-${month}-${date}`)
-
-                  // FIXME: undefined sometimes
-                  const leave = employee.leaves.find(
-                    leave =>
-                      new Date(leave.from) <= targetDate &&
-                      new Date(leave.to) >= targetDate
-                  )
-                  if (leave)
-                    return (
-                      <div
-                        className={
-                          leave.duration === 'fullday'
-                            ? 'bg-primary'
-                            : 'bg-secondary'
-                        }
-                        style={{ height: 50, width: 20 }}
-                      />
-                    )
-                  return <></>
-                })
+            employeeLeaves
+              .filter(({ id }) =>
+                self?.type === 'Employee' && self.employeeId
+                  ? id === self.employeeId
+                  : true
               )
-            )
+              .map(employee =>
+                [
+                  <Link
+                    role='button'
+                    to={
+                      ROUTES.leave.details.replace(
+                        ROUTES.leave._params.id,
+                        employee.id.toString()
+                      ) +
+                      '?' +
+                      new URLSearchParams({
+                        month: fromDateString
+                      } satisfies typeof ROUTES.leave._queries)
+                    }
+                    className='align-items-center d-flex gap-2 py-2 text-decoration-none'
+                  >
+                    <img
+                      src='/favicon.png'
+                      width='50'
+                      height='50'
+                      className='object-fit-cover rounded-circle'
+                    />
+                    <div>
+                      <p
+                        style={{ fontSize: 12 }}
+                        className='fw-lighter m-0 text-info'
+                      >
+                        {employee.email}
+                      </p>
+                      <p className='fw-bold m-0 text-nowrap'>{employee.name}</p>
+                      <p
+                        style={{ fontSize: 12 }}
+                        className='fw-lighter m-0 text-muted'
+                      >
+                        {employee.designation.name}
+                      </p>
+                    </div>
+                  </Link>
+                ].concat(
+                  calender.map(({ month, date }) => {
+                    const year =
+                      month === '01'
+                        ? toDate.getFullYear()
+                        : fromDate.getFullYear()
+                    const targetDate = stringToDate(`${year}-${month}-${date}`)
+
+                    // FIXME: undefined sometimes
+                    const leave = employee.leaves.find(
+                      leave =>
+                        stringToDate(leave.from) <= targetDate &&
+                        stringToDate(leave.to) >= targetDate
+                    )
+                    if (leave)
+                      return (
+                        <div
+                          className={
+                            leave.duration === 'fullday'
+                              ? 'bg-primary'
+                              : 'bg-secondary'
+                          }
+                          style={{ height: 50, width: 20 }}
+                        />
+                      )
+                    return <></>
+                  })
+                )
+              )
           )}
       />
     </>
