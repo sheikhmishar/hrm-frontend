5b8f1ae8e524ce18a606ea4c221f34052f3a56a2
sheikhmishar <misharex.sezan@gmail.com>
2024-12-15 11:54:42 +0600

    updated error boundary

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { type PropsWithChildren } from 'react'
 import type { ErrorInfo } from 'react'
 
 import Button from '../components/Button'
@@ -9,8 +9,8 @@ type State = {
   errorInfo?: ErrorInfo
 }
 
-class ErrorBoundary extends React.Component<any, State> {
-  constructor(props: any) {
+class ErrorBoundary extends React.Component<PropsWithChildren, State> {
+  constructor(props: PropsWithChildren) {
     super(props)
     this.state = { hasError: false, error: undefined, errorInfo: undefined }
   }
@@ -30,6 +30,9 @@ class ErrorBoundary extends React.Component<any, State> {
       return (
         <>
           <h3>Error Occured. Please Contact the developer</h3>
+          {import.meta.env.DEV && (
+            <h5 className='text-danger'>{this.state.error?.message}</h5>
+          )}
           <Button
             className='btn-danger'
             onClick={() => this.setState({ hasError: false })}
