0515962823c4581d7e7a2fc9f0c3045b4e952e8c
sheikhmishar <misharex.sezan@gmail.com>
2024-12-15 11:59:08 +0600

    added timeout end for select component dropdown

--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef, useMemo } from 'react'
 
-export default function useTimeout(
-  callback: (...args: any[]) => void,
+export default function useTimeout<T>(
+  callback: (...args: T[]) => void,
   delay: number
 ) {
   const callbackRef = useRef(callback)
@@ -15,10 +15,14 @@ export default function useTimeout(
   useEffect(() => () => clearTimeout(timerRef.current), [])
 
   const start = useCallback(
-    (...args: any[]) =>
+    (...args: T[]) =>
       (timerRef.current = setTimeout(callbackRef.current, delay, ...args)),
     [delay]
   )
+  const end = useCallback(() => {
+    clearTimeout(timerRef.current)
+    timerRef.current = undefined
+  }, [delay])
 
-  return useMemo(() => ({ start }), [start])
+  return useMemo(() => ({ start, end }), [start, end])
 }
